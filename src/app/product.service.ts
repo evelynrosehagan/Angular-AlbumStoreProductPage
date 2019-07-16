dd247f56b62726333ab2c6c6da87d3e0fb75c220
Evelyn Hagan <evelyn.hagan@avanade.com>
2019-07-16 14:38:11 -0400

    step 2 fixes 1

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,8 +8,8 @@ export class ProductService {
   private _albumUrl = "../assets/album.json";
   constructor(  private _http: Http) { }
 
-  Observable<Album> getAlbum(id : number)
+   getAlbum(id : number) : Observable<Album>
   {
-    return this._http.get(this._albumUrl).map((response)=>response.json);
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
 }
