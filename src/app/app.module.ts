86b4c35756846ac6ca0cb7667e65fda57325e8e3
Evelyn Hagan <evelyn.hagan@avanade.com>
2019-07-16 15:36:05 -0400

    lastfix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes);
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
